3ab00e5f0aa2275a7021c25fbd96d1d6e784c609
stefannitulescu <snitulescu2001@yahoo.com>
2024-03-11 16:02:00 +0200

    First commit

---
+++ src/main/java/com/licenta/licenta/data/dto/PersonDTO.java
@@ -0,0 +1,31 @@
+package com.licenta.licenta.data.dto;
+
+public class PersonDTO {
+    private String username;
+    private String email;
+    private String hobby;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+}
